f1a66ca0de5186cee565ae17be86de382aefc02c
zerozoo-front <zerozoo385@gmail.com>
2021-07-21 01:22:55 +0900

    add npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Nav from './components/Nav';
+import theme from '../assets/theme';
 const App = () => {
+  const [storage, setStorage] = useState('');
+
+  const onClick = () => {
+    localStorage.setItem('key', 'hohohaha');
+    console.log('local:', localStorage.getItem('key'));
+  };
+
   return (
     <>
+      {/* <ThemeProvider theme={theme}> */}
       <div>hello world!</div>
+      <button onClick={onClick}>button</button>
+      <Nav />
+      {/* </ThemeProvider> */}
     </>
   );
 };
---
+++ src/components/Nav.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import { useTheme } from '@material-ui/styles';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}>
+      {value === index && (
+        <Box p={1}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `tab-${index}`,
+    'aria-controls': `tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = useState(0);
+  console.log('theme: ', theme);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Tabs value={value} onChange={handleChange} aria-label='tabs'>
+          <Tab label='Item One' {...a11yProps(0)} />
+          <Tab label='Item Two' {...a11yProps(1)} />
+          <Tab label='Item Three' {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Three
+      </TabPanel>
+    </div>
+  );
+}
