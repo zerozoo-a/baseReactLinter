4a69a2c5713232a702add75d4def0d701b54652e
YOUNGJOO-YOON <zerozoo-front@gmail.com>
2021-07-28 16:51:11 +0900

    fix: path

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   plugins:[
      new CopyWebpackPlugin({
       patterns:[
-        {from:'./index.html', to:'../index.html'},
+        {from:'./index.html', to:'./index.html'},
       ]
     })
   ],
